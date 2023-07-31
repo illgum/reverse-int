ce615507649b71572452af16f2368d7ea08083ab
Ilgam Arslanov <ailham52@gmail.com>
2023-07-31 23:55:43 +0300

    feat: write the solution  code in index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+  let reversed = 0;
+
+  while (n !== 0) {
+    let digit = n % 10;
+    reversed = reversed * 10 + digit;
+    n = Math.floor(n / 10);
+  }
+  return reversed;
 }
